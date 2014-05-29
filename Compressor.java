188de32a06cb9781c26a1872a6959d0be9ee0f37
emretetik96 <scottetik@gmail.com>
2014-05-29 19:53:44 -0400

    Create Compressor.java

---
+++ Compressor.java
@@ -0,0 +1,16 @@
+public class Compressor {
+
+  double threshold;
+  double ratio; 
+  
+  public Compressor() {
+    threshold = null;
+    ratio = null;
+  }
+
+  public Compressor(double newThresh, double newRatio) {
+    threshold = newThresh;
+    ratio = newRatio;
+  }
+
+}
